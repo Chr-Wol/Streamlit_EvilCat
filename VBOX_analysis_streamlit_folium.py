edef8cf21fc0d951b00a9f41675dc75b2e3d2495
Chr-Wol <103592868+Chr-Wol@users.noreply.github.com>
2026-08-11 11:47:53 +0200

    Add files via upload

--- VBOX_analysis_streamlit_folium.py
+++ VBOX_analysis_streamlit_folium.py
@@ -233,7 +233,7 @@ if not st.session_state["data_loaded"]:
             ],
             icon=folium.DivIcon(
                 icon_size=(150, 30),
-                icon_anchor=(75, 15),
+                icon_anchor=(75, 40),
                 html="""
                 <div style="
                     font-size: 14px;
